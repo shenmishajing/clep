d9b10d486db9f57ec691adab3328f010fd1d0c2c
shenmishajing <shenmishajing@gmail.com>
2023-08-03 10:21:07 +0800

    add res9 and res4, use layers instead of layer1-4

--- models/resnet1d/resnet1d.py
+++ models/resnet1d/resnet1d.py
@@ -136,6 +136,8 @@ class Bottleneck(nn.Module):
 
 class ResNet1D(nn.Module):
     ARCHS = {
+        "res4": (BasicBlock, [1, 1]),
+        "res9": (BasicBlock, [2, 2]),
         "res18": (BasicBlock, [2, 2, 2, 2]),
         "res34": (BasicBlock, [3, 4, 6, 3]),
         "res50": (Bottleneck, [3, 4, 6, 3]),
@@ -178,6 +180,7 @@ class ResNet1D(nn.Module):
                 "replace_stride_with_dilation should be None "
                 f"or a 3-element tuple, got {replace_stride_with_dilation}"
             )
+        replace_stride_with_dilation = [False] + replace_stride_with_dilation
         self.groups = groups
         self.base_width = width_per_group
         self.conv1 = nn.Conv1d(
@@ -186,15 +189,17 @@ class ResNet1D(nn.Module):
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(
-            block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0]
-        )
-        self.layer3 = self._make_layer(
-            block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1]
-        )
-        self.layer4 = self._make_layer(
-            block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
+        self.layers = nn.Sequential(
+            *[
+                self._make_layer(
+                    block,
+                    2 ** (i + 6),
+                    layers[i],
+                    1 if i == 0 else 2,
+                    replace_stride_with_dilation[i],
+                )
+                for i in range(len(layers))
+            ]
         )
         self.avgpool = nn.AdaptiveAvgPool1d((1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
@@ -264,22 +269,21 @@ class ResNet1D(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def _forward_impl(self, x: Tensor) -> Tensor:
+    def extract_feat(self, x):
         # See note [TorchScript super()]
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        x = self.layers(x)
+        return x
 
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        x = self.extract_feat(x)
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
-
         return x
 
     def forward(self, data):
@@ -311,14 +315,7 @@ class ResNet1DEncoder(ResNet1D):
         # See note [TorchScript super()]
         batch_size, single_num = x.shape[:2]
         x = x.reshape(batch_size * single_num, -1, self.conv1.in_channels).mT
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        x = self.extract_feat(x)
 
         return x.reshape(batch_size, single_num, *x.shape[-2:]).mT

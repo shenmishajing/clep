44f78690269a9e8908bbb361e8cf7b9ff817416f
shenmishajing <shenmishajing@gmail.com>
2023-08-08 12:57:23 +0800

    add conv clep config file, fix init bug, fix symbols bug, add 9x schedule

--- models/clep/__init__.py
+++ models/clep/__init__.py
@@ -1,3 +1,4 @@
 from .clep import CLEP as CLEP
+from .conv_clep import ConvCLEP as ConvCLEP
 from .ecg_conv_transformer import ECGConvTransformer as ECGConvTransformer
 from .ecg_transformer import ECGTransformer as ECGTransformer
